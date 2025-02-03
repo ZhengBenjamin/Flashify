0c715432379e7449fc162ebe63e626ae1be8ad46
Benjamin Zheng <54424912+ZhengBenjamin@users.noreply.github.com>
2025-02-03 15:03:29 -0500

    Frontend UI (#3)
    * Added Login Page
    
    * Added sign up card

---
+++ client/src/assets/components/jsx/RegisterCard.jsx
@@ -0,0 +1,29 @@
+import {
+  Anchor,
+  Button,
+  Group,
+  Paper,
+  PasswordInput,
+  TextInput,
+} from '@mantine/core';
+
+export default function LoginCard() {
+  return (
+    <div>
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Username" placeholder="Username" required />
+        <TextInput label="Email" placeholder="example@flashify.com" required mt="md" />
+        <PasswordInput label="Password" placeholder="Password" required mt="md" />
+        <PasswordInput label="Repeat Password" placeholder="Repeat Password" required mt="md" />
+        <Group justify="space-between" mt="lg">
+          <Anchor component="button" size="sm">
+            Already have an account?
+          </Anchor>
+        </Group>
+        <Button fullWidth mt="xl">
+          Sign Up
+        </Button>
+      </Paper>
+    </div>
+  );
+}
