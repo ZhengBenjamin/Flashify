2c3a80076654ebbdd381d299dde52e9daeabdc31
Ari Glockner <ari@glockners.net>
2025-02-10 19:00:22 -0500

    Added flashcards to the set

---
+++ client/src/assets/components/jsx/Flashcards.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { Card, Container, Group, Text, Button } from '@mantine/core';
+import classes from '../css/Flashcards.module.css';
+
+const flashcardsData = [
+    { front: 'Front of Card 1', back: 'Back of Card 1' },
+    { front: 'Front of Card 2', back: 'Back of Card 2' },
+    { front: 'Front of Card 3', back: 'Back of Card 3' },
+];
+
+export default function Flashcards() {
+    const [currentCardIndex, setCurrentCardIndex] = useState(0);
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlip = () => {
+        setIsFlipped(!isFlipped);
+    };
+
+    const handleNext = () => {
+        setIsFlipped(false);
+        setCurrentCardIndex((prevIndex) => (prevIndex + 1) % flashcardsData.length);
+    };
+
+    const handlePrev = () => {
+        setIsFlipped(false);
+        setCurrentCardIndex((prevIndex) => (prevIndex - 1 + flashcardsData.length) % flashcardsData.length);
+    };
+
+    const currentCard = flashcardsData[currentCardIndex];
+
+    return (
+        <Container className={classes.container}>
+            <Group position="center">
+                <Card className={classes.card} onClick={handleFlip}>
+                    <Text>{isFlipped ? currentCard.back : currentCard.front}</Text>
+                </Card>
+            </Group>
+            <Group position="center" mt="md">
+                <Button onClick={handlePrev}>Previous</Button>
+                <Button onClick={handleNext}>Next</Button>
+            </Group>
+        </Container>
+    );
+}
