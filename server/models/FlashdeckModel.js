a5e3cb6737505b0a66c5ce6b1f9c7db332f049af
FrantzFrankenStein <fmw13@case.edu>
2025-03-06 21:23:33 -0500

    updated schemas

--- server/models/FlashdeckModel.js
+++ server/models/FlashdeckModel.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
+const Card = require("../models/FlashcardModel");
+
 
 const FlashdeckSchema = new mongoose.Schema({
 
 deck_id: { type: String, required: true, unique: true },
-username: { type: String, required: true, unique: true },
+username: { type: String, required: true },
 title: { type: String, required: true },
 description: { type: String, default: "Null" },
 card_count: {type: Number, required: true },
-cards: {type: [Cards], default: undefined}
+cards: {type: [{ type: mongoose.Schema.Types.ObjectId, ref: Card }] }
 
 });
 
